14463291dece12de81576ca4ccd62142dee9c632
Katie Macalintal <kmacalintal@middlebury.edu>
2022-10-04 15:45:16 -0400

    beginning to write read

--- myio.c
+++ myio.c
@@ -5,15 +5,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "myio.h"
 
 #include <fcntl.h> 
 
-#define BYTES_PER_ITERATION 10240
+#define BUFF_SIZE 5
+
+char *READ_BPTR; 
+char *READ_CPTR; 
 
 int main(int argc, char *argv[])
 {
     printf("TESTING \n"); 
+    myopen("/home/kmacalintal/CS315/assignment2/testfile", 0);  
+
     return 0; 
 }
 
@@ -22,6 +28,9 @@ int main(int argc, char *argv[])
 */
 int myopen(const char *pathname, int flags) 
 {
+    READ_BPTR = malloc(BUFF_SIZE); 
+    READ_CPTR = READ_BPTR; 
+    open(pathname, flags);
     return 0; 
 }
 
@@ -34,11 +43,23 @@ int myclose(int fd)
 }
 
 /*
-* Reads count bytes from fd into the buf 
+* Reads count bytes from fd into the buf and returns the amount of bytes read
 */
 int myread(int fd, void *buf, size_t count) 
 {
-    return 0; 
+    int amtRead; /*offset?*/
+
+    buf = READ_CPTR; 
+
+    if (READ_BPTR == READ_CPTR | READ_CPTR + count - READ_BPTR >= BUFF_SIZE) {
+        amtRead = read(fd, READ_BPTR, count); 
+    } 
+    READ_CPTR += amtRead; 
+
+    printf("%s", *(READ_BPTR));  
+    
+
+    return amtRead;
 }
 
 /*
