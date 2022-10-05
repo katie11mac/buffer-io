9740c60c15ea7372994f115a8195b12e043e5b31
Katie Macalintal <kmacalintal@middlebury.edu>
2022-10-05 16:51:27 -0400

    reset code

--- myio.c
+++ myio.c
@@ -17,9 +17,6 @@ char *READ_CPTR;
 
 int main(int argc, char *argv[])
 {
-    printf("TESTING \n"); 
-    myopen("/home/kmacalintal/CS315/assignment2/testfile", 0);  
-
     return 0; 
 }
 
@@ -28,9 +25,6 @@ int main(int argc, char *argv[])
 */
 int myopen(const char *pathname, int flags) 
 {
-    READ_BPTR = malloc(BUFF_SIZE); 
-    READ_CPTR = READ_BPTR; 
-    open(pathname, flags);
     return 0; 
 }
 
@@ -47,19 +41,7 @@ int myclose(int fd)
 */
 int myread(int fd, void *buf, size_t count) 
 {
-    int amtRead; /*offset?*/
-
-    buf = READ_CPTR; 
-
-    if (READ_BPTR == READ_CPTR | READ_CPTR + count - READ_BPTR >= BUFF_SIZE) {
-        amtRead = read(fd, READ_BPTR, count); 
-    } 
-    READ_CPTR += amtRead; 
-
-    printf("%s", *(READ_BPTR));  
-    
-
-    return amtRead;
+    return 0; 
 }
 
 /*
