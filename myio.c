2ca182d92f308be9786f96dd35226362ca5c0335
Schweitzer <lschweitzer@middlebury.edu>
2022-10-13 15:23:53 -0400

    finished write

--- myio.c
+++ myio.c
@@ -11,7 +11,7 @@
 
 #include <fcntl.h>
 
-#define BUFF_SIZE 5
+#define BUFF_SIZE 10
 /*
  buf_size and struct file should be in the header
 */
@@ -29,24 +29,32 @@ struct File
 int main(int argc, char *argv[])
 {
     struct File *filePtr;
-    char * buf;
+    struct File *filePtr2;
+    char *readBuf;
+    char *writeBuf;
     int results; 
 
-    buf = malloc(30); 
+    readBuf = malloc(30); 
     filePtr = myopen("testfile",O_RDWR);
-    printf("fd is %d buf pointer value is %p and readCP pointer value is %p \n",filePtr->fd, filePtr->readBuf, filePtr->readCP);
+    printf("fd is %d readBuf pointer value is %p and readCP pointer value is %p \n",filePtr->fd, filePtr->readBuf, filePtr->readCP);
     
-    results = myread(filePtr, buf, 2); // NEED TO TEST IT OUT WHEN YOU REQUEST MORE BYTES THAN THE FILE HAS 
-    printf("***this is whats in the buf at %p: %s***\n", buf, buf); 
-    printf("bytes read: %d\n\n", results); 
+    // results = myread(filePtr, readBuf, 2); // NEED TO TEST IT OUT WHEN YOU REQUEST MORE BYTES THAN THE FILE HAS 
+    // printf("***this is whats in the readBuf at %p: %s***\n", readBuf, readBuf); 
+    // printf("bytes read: %d\n\n", results); 
 
-    results = myread(filePtr, buf, 30); // NEED TO TEST IT OUT WHEN YOU REQUEST MORE BYTES THAN THE FILE HAS 
-    printf("***this is whats in the buf at %p: %s***\n", buf, buf); 
-    printf("bytes read: %d\n\n", results); 
+    // results = myread(filePtr, readBuf, 30); // NEED TO TEST IT OUT WHEN YOU REQUEST MORE BYTES THAN THE FILE HAS 
+    // printf("***this is whats in the readBuf at %p: %s***\n", readBuf, readBuf); 
+    // printf("bytes read: %d\n\n", results); 
     
-    results = myread(filePtr, buf, 30); // NEED TO TEST IT OUT WHEN YOU REQUEST MORE BYTES THAN THE FILE HAS 
-    printf("***this is whats in the buf at %p: %s***\n", buf, buf); 
-    printf("bytes read: %d\n\n", results); 
+    // results = myread(filePtr, readBuf, 30); // NEED TO TEST IT OUT WHEN YOU REQUEST MORE BYTES THAN THE FILE HAS 
+    // printf("***this is whats in the readBuf at %p: %s***\n", readBuf, readBuf); 
+    // printf("bytes read: %d\n\n", results); 
+
+    writeBuf = "If we see this, we wrote correctly!";
+    filePtr2 = myopen("writeTestFile",O_RDWR);
+    mywrite(filePtr2, writeBuf, 5);
+    mywrite(filePtr2, writeBuf+5, 7);
+    mywrite(filePtr2, writeBuf+12, 10);
 
     return 0; 
 }
@@ -249,10 +257,9 @@ int myread(struct File *filePtr, char *buf, size_t count)
 /*
 * Writes count bytes from buf into the fd 
 */
-int mywrite(struct File *filePtr, const void *buf, size_t count)
+int mywrite(struct File *filePtr, void *buf, size_t count)
 {
-    int canWrite;
-
+    int canWrite, bytesLeft;
 
     canWrite = 0;
 
@@ -267,15 +274,42 @@ int mywrite(struct File *filePtr, const void *buf, size_t count)
         printf("CANNOT WRITE\n"); 
         return 0;
     }
-    
-    while(count > 0)
+
+    bytesLeft = BUFF_SIZE - (filePtr->writeCP - filePtr->writeBuf);
+
+    // if count is smaller than writeBuf we write to writeBuf until full
+    if(count < BUFF_SIZE)
+    {    
+        printf("bytesLeft: %d\n", bytesLeft); 
+        // Case when count is less than size left in our writeBuf
+        if(count <= bytesLeft)
+        {
+            printf("RUNNING THE IF\n"); 
+            memcpy(filePtr->writeCP, buf, count);
+            printf("***this is whats in the writeBuf at %p: %s***\n", filePtr->writeCP, filePtr->writeCP); 
+            filePtr->writeCP += count;
+            count = 0;
+        }
+        // Case when count is greater the size left in our writeBuf
+        else
+        {
+            printf("RUNNING THE ELSE\n"); 
+            memcpy(filePtr->writeCP, buf, bytesLeft);
+            write(filePtr->fd, filePtr->writeBuf, BUFF_SIZE);
+            filePtr->writeCP = filePtr->writeBuf;
+            count -= bytesLeft;
+        }
+    }
+    // if count is bigger than writeBuf we do syscall automatically
+    else
     {
-    
+        // write whats in writeBuf to file
+        write(filePtr->fd, filePtr->writeBuf, BUFF_SIZE-bytesLeft);
+        printf("this is how much we are writing: %d\n",BUFF_SIZE-bytesLeft);
+        filePtr->writeCP = filePtr->writeBuf;
 
-
-
-
-
+        //write count from buff to file
+        write(filePtr->fd, buf, count);
     }
     
     return 0; 
