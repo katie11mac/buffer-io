de2ca00f001663b4e915f1ce27e38a93c4b858c5
Schweitzer <lschweitzer@middlebury.edu>
2022-10-13 16:22:00 -0400

    started to fix myread

--- myio.c
+++ myio.c
@@ -104,13 +104,9 @@ int myclose(int fd)
 */
 int myread(struct File *filePtr, char *buf, size_t count)
 { 
-    int canRead, bytesRead;
-    size_t bytesLeft, originalCount;
-    originalCount = count; 
+    int canRead; 
 
     canRead = 0;
-
-    printf("\ninital buf: %p \tinitial count: %ld\n", buf, count);
 
     if(((filePtr->flags & O_RDONLY) != 0) || ((filePtr->flags & O_RDWR) != 0))
     {
@@ -124,134 +120,6 @@ int myread(struct File *filePtr, char *buf, size_t count)
         return 0;
     }
 
-    do 
-    {
-        /*CASE 1: File buf is empty*/
-        if(filePtr->readCP == NULL)
-        {
-            printf("\nFILLING IN FILE BUF\n"); 
-            bytesRead = read(filePtr->fd, filePtr->readBuf, BUFF_SIZE); 
-            printf("\tRead %d bytes\n", bytesRead); 
-            printf("\tFile buf value: \'%s\'\n", filePtr->readBuf); 
-            filePtr->readCP = filePtr->readBuf;
-            if(bytesRead == -1)
-            {
-                perror("read");
-                return -1;
-            }
-            else if(bytesRead == 0)
-            {
-                printf("End of file\n");
-                return 0;
-            }
-        }
-
-        if(bytesRead < BUFF_SIZE)
-        {
-            bytesLeft = bytesRead - ((filePtr->readCP) - (filePtr->readBuf)); 
-        }
-        else
-        {
-            bytesLeft = BUFF_SIZE - ((filePtr->readCP) - (filePtr->readBuf));
-        }
-         
-        /*CASE 2: User requests more bytes than what is left in the File buf*/
-        if(bytesLeft < count)
-        {
-            printf("\nUSER REQUESTED MORE BYTES THAN WHAT IS IN FILE BUF\n"); 
-            if(memcpy(buf, filePtr->readCP, bytesLeft) == NULL)
-            {
-                printf("MEMCPY NULL (1)\n"); 
-                return -1;
-            }
-            printf("\tCurrent User Buf Value (from %p): %s\n", buf, buf); 
-            count -= bytesLeft;
-            buf += bytesLeft; /*change where we are writing to in the user's buf*/
-            printf("\tupdated buf: %p \tupdated count: %ld\n", buf, count); 
-            printf("\tREFILL FILE BUFF\n"); 
-            bytesRead = read(filePtr->fd, filePtr->readBuf, BUFF_SIZE);
-            printf("\tRead %d bytes\n", bytesRead); 
-            printf("\tFile buf value: \'%s\'\n", filePtr->readBuf); 
-            if(bytesRead == -1)
-            {
-                perror("read");
-                return -1;
-            }
-            else if(bytesRead == 0)
-            {
-                printf("End of file\n");
-                // bytesLeft = 0; 
-                bytesLeft = bytesRead - ((filePtr->readCP) - (filePtr->readBuf)); 
-                //return 0; return original count - count 
-            }
-            filePtr->readCP = filePtr->readBuf; /*need to reset the readCP pointer*/
-            printf("\t \n"); 
-        }
-        
-
-        /*CASE 3: Can provide the bytes that are left in the File buf*/
-        if (count <= bytesLeft) 
-        {
-            printf("\nPROVIDE USER WITH CONTENTS THAT ARE IN FILE BUF (MEMCPY-ING)\n"); 
-            printf("count = %ld\n",count);
-            printf("bytesLeft = %ld\n",bytesLeft);
-            printf("bytesRead = %d\n",bytesRead);
-            
-            // if(bytesRead < BUFF_SIZE) // means you have read up to the end of the file because you weren't able to fill the entire buff
-            // {
-            //     printf("\tbytesRead < BUFF_SIZE\n"); 
-            //     if(memcpy(buf, filePtr->readCP, bytesRead) == NULL)
-            //     {
-            //         printf("MEMCPY NULL (2)\n");
-            //         return -1;
-            //     }
-            //     printf("\tCurrent User Buf Value (from %p): %s\n", buf, buf);
-            //     filePtr->readCP += bytesRead;
-            //     count -= bytesRead;
-            //     bytesRead -= bytesRead; 
-            // } 
-            // else 
-            // {
-                if(memcpy(buf, filePtr->readCP, count) == NULL)
-                {
-                    printf("MEMCPY NULL (2)\n");
-                    return -1;
-                }
-                printf("\tCurrent User Buf Value (from %p): %s\n", buf, buf);
-                filePtr->readCP += count;
-                count -= count;
-                bytesRead -= count; 
-            // }
-
-            printf("\ncount = %ld\n", count);
-            printf("\n"); 
-        }
-        
-    } 
-    while(count > 0 && bytesRead == BUFF_SIZE); 
-
-
-    if (count == 0)
-    {
-        
-        return originalCount; 
-    } 
-    else // weren't able to reach requested amount, so try to mem cpy one last time with the number of bytes read 
-    {
-        // printf("QUICK MEMCPY\n"); 
-        // printf("\tbytesRead: %d\n", bytesRead); 
-        // if(memcpy(buf, filePtr->readCP, bytesRead) == NULL)
-        // {
-        //     {
-        //         printf("MEMCPY NULL (2)\n");
-        //         return -1;
-        //     }
-        // }
-        // printf("\tCurrent User Buf Value (from %p): %s\n", buf, buf);
-        // filePtr->readCP += bytesRead; //do we need to change something here
-        // count -= bytesRead;
-        return (originalCount - count); 
-    }
     
 }
 /*
